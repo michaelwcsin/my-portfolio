65aea74076e4d82678df2e99fe2d7283f88cea9f
michaelwcsin <michael.wc.sin@gmail.com>
2024-10-05 04:58:07 -0600

    Changed delay on flip words to flip as soon as text generate finishes

--- data/datapoints.tsx
+++ data/datapoints.tsx
@@ -103,8 +103,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
+    title: "Currently building this portfolio.",
+    description: "",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
@@ -113,7 +113,7 @@ export const gridItems = [
   },
   {
     id: 6,
-    title: "Do you want to start a project together?",
+    title: "Let's collaborate!",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
