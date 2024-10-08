642da9ad82c727ac50e1fe10a73787cef5d43ef6
michaelwcsin <michael.wc.sin@gmail.com>
2024-10-07 21:45:43 -0600

    Changes to files

--- components/ui/CollisionBeams.tsx
+++ components/ui/CollisionBeams.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "h-screen md:h-screen from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative flex items-center w-full justify-center overflow-hidden",
+        "h-screen md:h-screen bg-opacity-100 from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative flex items-center w-[59vw] justify-center overflow-hidden",
         // h-screen if you want bigger
         className
       )}
@@ -85,14 +85,6 @@ export const BackgroundBeamsWithCollision = ({
       ))}
 
       {children}
-      <div
-        ref={containerRef}
-        className="absolute bottom-0 bg-neutral-100 w-full inset-x-0 pointer-events-none"
-        style={{
-          boxShadow:
-            "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset",
-        }}
-      ></div>
     </div>
   );
 };
