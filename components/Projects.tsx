c85c73f2c62e88439e9eed00e6118ad9eeb6c727
michaelwcsin <michael.wc.sin@gmail.com>
2024-10-09 23:17:36 -0600

    Deleted link to pdf

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -56,13 +56,6 @@ const Projects = () => {
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
-        <a
-          href={process.env.PUBLIC_URL + "/prospect_document.pdf"}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <button>Pdf</button>
-        </a>
       </div>
     </div>
   );
